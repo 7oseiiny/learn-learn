41dfd78dad656ef629f1b511e98cf0a87f93b677
ahmed elhoseiny <ahmedelhoseiny5555@gmail.com>
2025-08-03 21:46:21 +0300

    f

--- e-learning-front/src/app/auth/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div className="max-w-md mx-auto mt-12 p-6 bg-white rounded-xl shadow">
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
-      {/* هنا هنضيف الفورم لاحقًا */}
-    </div>
-  )
-}
