113898e56aa2a8bb82cc5f4256898279c3657ac5
ahmed elhoseiny <ahmedelhoseiny5555@gmail.com>
2025-09-05 22:13:15 +0300

    handle buttons status

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Welcome: React.FC = () => (
-    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
-        <h1>Welcome to Learn-Learn!</h1>
-        <p>This is the welcome page.</p>
-        {/* <div style={{ marginTop: '2rem' }}>
-            <Link to="/signin" style={{ marginRight: '1rem' }}>Sign In</Link>
-            <Link to="/signup">Sign Up</Link>
-        </div> */}
-    </div>
-);
+
+const Welcome: React.FC = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+    useEffect(() => {
+        const checkLogin = () => setIsLoggedIn(!!localStorage.getItem('token'));
+        window.addEventListener('storage', checkLogin);
+        window.addEventListener('authChange', checkLogin);
+        return () => {
+            window.removeEventListener('storage', checkLogin);
+            window.removeEventListener('authChange', checkLogin);
+        };
+    }, []);
+    return (
+        <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+            <h1>Welcome to Learn-Learn!</h1>
+            <p>This is the welcome page.</p>
+            {!isLoggedIn && (
+                <div style={{ marginTop: '2rem' }}>
+                    <Link to="/signin" style={{ marginRight: '1rem' }}>Sign In</Link>
+                    <Link to="/signup">Sign Up</Link>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default Welcome;
